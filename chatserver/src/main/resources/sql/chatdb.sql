87820f33fea8d47bfc4eb0c1361e93b140ae6231
Linshowtime <1394901893@qq.com>
2018-06-21 00:40:17 +0800

    数据库优化： 在数据库表中添加索引

--- chatserver/src/main/resources/sql/chatdb.sql
+++ chatserver/src/main/resources/sql/chatdb.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50721
 File Encoding         : 65001
 
-Date: 2018-06-20 23:18:25
+Date: 2018-06-21 00:33:27
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -22,7 +22,8 @@ DROP TABLE IF EXISTS `chat_group`;
 CREATE TABLE `chat_group` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(40) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -36,7 +37,8 @@ CREATE TABLE `chat_message` (
   `time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `content` varchar(500) DEFAULT NULL,
   `is_read` tinyint(4) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `sender_receiver` (`sender_id`,`receiver_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -51,7 +53,9 @@ CREATE TABLE `contact_invation` (
   `tag` int(11) NOT NULL DEFAULT '0' COMMENT '0表示未发送，1表示已发送',
   `category_id` int(11) DEFAULT NULL,
   `message` varchar(255) DEFAULT '' COMMENT '添加好友的备注信息',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `contact_user_id` (`contact_user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -62,7 +66,8 @@ CREATE TABLE `friend_category` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(40) DEFAULT NULL COMMENT '分组名程',
   `owner_id` int(11) DEFAULT NULL COMMENT '分组拥有者ID',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `owner_id` (`owner_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -76,7 +81,7 @@ CREATE TABLE `friend_category_member` (
   `member_id` int(11) DEFAULT NULL,
   `alia_name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `category_id` (`category_id`)
+  KEY `owner_member` (`owner_id`,`member_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -90,7 +95,8 @@ CREATE TABLE `group_message` (
   `time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `content` varchar(500) DEFAULT NULL,
   `type` int(11) DEFAULT NULL COMMENT '0表示普通信息，1表示公告',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `group_sender` (`group_id`,`sender_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -101,8 +107,7 @@ CREATE TABLE `group_user` (
   `group_id` int(11) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
   `max_message_id` int(11) DEFAULT NULL,
-  KEY `group_id` (`group_id`),
-  KEY `user_id` (`user_id`)
+  KEY `group_user_id` (`group_id`,`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -122,5 +127,6 @@ CREATE TABLE `user_t` (
   `gender` int(11) DEFAULT NULL,
   `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `update_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `username` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
