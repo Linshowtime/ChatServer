d8996d971ad5d7ce8906752ca00c50063cafde9a
Linshowtime <1394901893@qq.com>
2018-06-22 14:24:45 +0800

    修复群未读信息bug

--- chatserver/src/main/java/com/nt/service/impl/GroupMessageServiceImpl.java
+++ chatserver/src/main/java/com/nt/service/impl/GroupMessageServiceImpl.java
@@ -33,6 +33,6 @@ public class GroupMessageServiceImpl implements IGroupMessageService {
 	@Override
 	public List<GroupMessage> findUnreadMessages(Integer groupId, Integer maxId) {
 		
-		return null;
+		return mapper.selectUnreadMessages(groupId, maxId);
 	}
 }
